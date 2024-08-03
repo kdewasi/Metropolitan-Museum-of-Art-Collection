c67360f921ffbcf551b1871d53c749c4eb1f33e0
kdewasi <139508236+kdewasi@users.noreply.github.com>
2024-08-02 21:49:44 -0400

    Update [objectID].js

--- pages/artwork/[objectID].js
+++ pages/artwork/[objectID].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { Row, Col } from 'react-bootstrap';
-import ArtworkCardDetail from '../../components/ArtworkCardDetail';
+import ArtworkCardDetail from '@/components/ArtworkcardDetail';
 
 export default function ArtworkById() {
   const router = useRouter();
